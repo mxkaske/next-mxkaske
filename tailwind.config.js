103e08f05a27d07b5bc6e374c5ef3be148241623
Maximilian Kaske <maximilian@kaske.org>
2021-01-05 19:58:43 +0100

    :art: add tailwindcss

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+const colors = require("tailwindcss/colors");
+
+module.exports = {
+  purge: ["./pages/**/*.tsx", "./components/**/*.tsx"],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    colors: {
+      primary: colors.indigo,
+      secondary: colors.yellow,
+      neutral: colors.gray,
+    },
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+};
