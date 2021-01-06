f367c1f8a4a3792c361e996ef046e68fc84ba606
Maximilian Kaske <maximilian@kaske.org>
2021-01-07 00:02:48 +0100

    :wrench: footer alignment example

--- components/footer.tsx
+++ components/footer.tsx
@@ -1,15 +1,34 @@
 import React from "react";
 import Container from "./container";
 import Button from "./button";
+import Tag from "./tag";
 
 const Footer = () => {
   return (
-    <footer className="bg-primary-200 py-4">
-      <Container>
-        <Button className="block pb-2">LinkedIn</Button>
-        <Button className="block pb-2">Twitter</Button>
-        <Button className="block pb-2">GitHub</Button>
-        <Button className="block">Hashnode</Button>
+    <footer className="bg-primary-200">
+      <Container className="flex flex-col sm:flex-row space-between">
+        <div className="flex-1">
+          <Button className="block pb-2">LinkedIn</Button>
+          <Button className="block pb-2">Twitter</Button>
+          <Button className="block pb-2">GitHub</Button>
+          <Button className="block">Hashnode</Button>
+        </div>
+        <div className="flex flex-col items-end justify-end text-xs">
+          <p className="pb-1">
+            designed by
+            <Tag label="mxkaske" className="ml-1" />
+          </p>
+          <p className="pb-1">
+            build with
+            <Tag label="nextjs" className="ml-1" />
+          </p>
+          <Tag label="tailwindcss" className="block mb-1" />
+          <Tag label="framer" className="block mb-1" />
+          <p className="pb-1">
+            deployed on
+            <Tag label="vercel" className="ml-1" />
+          </p>
+        </div>
       </Container>
     </footer>
   );
