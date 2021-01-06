82df1272571ad09a95405dbc0cd2cbfc43cc507f
Maximilian Kaske <maximilian@kaske.org>
2021-01-06 23:59:57 +0100

    :dizzy: add animation to navbar

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -1,11 +1,38 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { FiTwitter, FiGithub } from "react-icons/fi";
+import { motion, useAnimation, Variants } from "framer-motion";
 import Button from "./button";
 import Container from "./container";
+import { useDisplayHeader } from "../hooks";
 
 const NavBar = () => {
+  const displayHeader = useDisplayHeader();
+  const controls = useAnimation();
+
+  useEffect(() => {
+    return () => {
+      displayHeader.onChange((e) => {
+        if (e) controls.start("visible");
+        else controls.start("hidden");
+      });
+    };
+  }, []);
+
+  const container: Variants = {
+    visible: { y: 0 },
+    hidden: { y: -60 },
+  };
+
   return (
-    <header className="w-full bg-primary-600 p-4">
+    <motion.header
+      className="w-full bg-primary-600 fixed top-0"
+      variants={container}
+      animate={controls}
+      initial="hidden"
+      transition={{
+        type: "tween",
+      }}
+    >
       <Container className="flex justify-between items-center">
         <h3 className="text-white">mxkaske</h3>
         <div className="flex">
@@ -17,7 +44,7 @@ const NavBar = () => {
           </Button>
         </div>
       </Container>
-    </header>
+    </motion.header>
   );
 };
 
