a51f3f932b47e058396f9321ca960c69748091e8
Maximilian Kaske <maximilian@kaske.org>
2021-01-07 21:08:17 +0100

    :art: add first tailwind component and variants to button component

--- components/button.tsx
+++ components/button.tsx
@@ -3,9 +3,30 @@ import React, { HTMLAttributes, ReactNode } from "react";
 interface ButtonProps extends HTMLAttributes<HTMLButtonElement> {
   label?: string;
   children?: ReactNode;
+  variant: "default" | "primary" | "secondary";
 }
-const Button = ({ children, label, ...props }: ButtonProps) => {
-  return <button {...props}>{label || children}</button>;
+const Button = ({
+  children,
+  label,
+  variant,
+  className,
+  ...props
+}: ButtonProps) => {
+  const btnVariant =
+    variant === "primary"
+      ? "btn-primary"
+      : variant === "secondary"
+      ? "btn-secondary"
+      : "";
+  return (
+    <button className={`${btnVariant} ${className || ""}`} {...props}>
+      {label || children}
+    </button>
+  );
+};
+
+Button.defaultProps = {
+  variant: "default",
 };
 
 export default Button;
--- tailwind.config.js
+++ tailwind.config.js
@@ -9,8 +9,15 @@ module.exports = {
       secondary: colors.yellow,
       neutral: colors.gray,
       white: colors.white,
+      black: colors.gray[700],
     },
     extend: {
+      height: {
+        112: "28rem",
+        128: "32rem",
+        144: "36rem",
+        160: "40rem",
+      },
       width: {
         "(1/2-8)": "calc(50% - 2rem)",
       },
