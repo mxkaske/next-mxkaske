164acd6ef15f46a9810e6623a5e2f7fb4af0fc34
Maximilian Kaske <maximilian@kaske.org>
2021-01-08 00:05:27 +0100

    :wrench: update config + pages and add svg to cover

--- config.ts
+++ config.ts
@@ -1,10 +1,27 @@
-import { ThumbnailProps } from "./components";
+import { TagProps, ThumbnailProps } from "./components";
 import faker from "faker";
 
 export const thumbnails: ThumbnailProps[] = [
   {
     title: "Hello World!",
     description: faker.lorem.paragraphs(2),
+    img: {
+      source: faker.image.nature(),
+      height: 400,
+      width: 400,
+    },
+    tags: [
+      {
+        label: "hello",
+      },
+      {
+        label: "world",
+      },
+    ],
+  },
+  {
+    title: "Hello World!",
+    description: faker.lorem.paragraph(2),
     img: {
       source: faker.image.business(),
       height: 400,
@@ -37,3 +54,69 @@ export const thumbnails: ThumbnailProps[] = [
     ],
   },
 ];
+
+type TechStackProps = {
+  web: TagProps[];
+  mobile: TagProps[];
+  backend: TagProps[];
+};
+
+export const techStack: TechStackProps = {
+  mobile: [
+    {
+      label: "react-native",
+      onClick: () => window.open("https://reactnative.dev/", "_blank"),
+    },
+    {
+      label: "expo",
+      onClick: () => window.open("https://expo.io/", "_blank"),
+    },
+    {
+      label: "redux",
+      onClick: () => window.open("https://redux.js.org/", "_blank"),
+    },
+    {
+      label: "@shopify/restyle",
+      onClick: () =>
+        window.open("https://github.com/Shopify/restyle", "_blank"),
+    },
+    {
+      label: "swmansion's stack",
+      onClick: () =>
+        window.open("https://swmansion.com/community/open-source/", "_blank"),
+    },
+  ],
+  web: [
+    {
+      label: "react",
+      onClick: () => window.open("https://reactjs.org/", "_blank"),
+    },
+    {
+      label: "react-query",
+      onClick: () => window.open("https://react-query.tanstack.com/", "_blank"),
+    },
+    {
+      label: "nextjs",
+      onClick: () => window.open("https://nextjs.org/", "_blank"),
+    },
+    {
+      label: "tailwindcss",
+      onClick: () => window.open("https://tailwindcss.com/", "_blank"),
+    },
+    {
+      label: "framer",
+      onClick: () =>
+        window.open("https://www.framer.com/api/motion/", "_blank"),
+    },
+  ],
+  backend: [
+    {
+      label: "4D",
+      onClick: () => window.open("https://developer.4d.com/docs/"),
+    },
+    {
+      label: "NodeJS",
+      onClick: () => window.open("https://nodejs.org/en/", "_blank"),
+    },
+  ],
+};
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,14 @@
 import Head from "next/head";
-import { Button, Container, Footer, NavBar, Thumbnail } from "../components";
+import {
+  Button,
+  Container,
+  Cover,
+  Footer,
+  NavBar,
+  TechStack,
+  Thumbnail,
+} from "../components";
+import Emoji from "../components/emoji";
 import { thumbnails } from "../config";
 
 export default function Home() {
@@ -10,16 +19,28 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <NavBar />
-      <Container>
-        <h1>Hello World!</h1>
-        <h2>Hello World!</h2>
-        <h3>Hello World!</h3>
-        <p>Hello World!</p>
-        <Button>Hello World!</Button>
-        <div className="flex flex-wrap -mx-4">
-          {[...thumbnails, ...thumbnails].map((thumbnail, idx) => (
-            <Thumbnail key={idx} {...thumbnail} />
-          ))}
+      <Cover />
+      <Container className="divide-y divide-primary-600">
+        <div className="py-4">
+          <h1>Hello World!</h1>
+          <h2>Hello World!</h2>
+          <h3>Hello World!</h3>
+          <p>Hello World!</p>
+          <Button>Hello World!</Button>
+        </div>
+        <div className="py-4">
+          <h2>
+            Project Highlights <Emoji symbol="🚀" label="rocket" />
+          </h2>
+          <div className="flex flex-wrap -mx-4">
+            {thumbnails.map((thumbnail, idx) => (
+              <Thumbnail key={idx} {...thumbnail} />
+            ))}
+            <div className="flex w-full md:w-(1/2-8) m-4 rounded-3xl border border-primary-900"></div>
+          </div>
+        </div>
+        <div className="py-4">
+          <TechStack />
         </div>
       </Container>
       <Footer />
