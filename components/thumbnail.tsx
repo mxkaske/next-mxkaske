dfd20d4a6548108c2ade153b2a70573eb66aaa4c
Maximilian Kaske <maximilian@kaske.org>
2021-01-10 15:14:41 +0100

    :art: style and props

--- components/thumbnail.tsx
+++ components/thumbnail.tsx
@@ -3,46 +3,57 @@ import Tag, { TagProps } from "./tag";
 
 export interface ThumbnailProps extends HTMLAttributes<HTMLDivElement> {
   title?: string;
+  subtitle?: string;
   description?: string;
+  annotation?: string;
   img?: {
     source: string;
     height: number;
     width: number;
+  };
+  link?: {
+    label: string;
+    href: string;
   };
   tags?: TagProps[];
 }
 
 const Thumbnail = ({
   title,
+  subtitle,
   description,
+  annotation,
   img,
   tags,
+  link,
   className,
   ...props
 }: ThumbnailProps) => {
-  const aspectRatio = img.width / img.height;
   return (
     <div
-      // flex flex-col flex-1 lg:flex-row
-      className={`w-full md:w-(1/2-8) m-4 ${className || ""}`}
+      className={`flex flex-col w-full md:w-(1/2-8) m-4 rounded-xl p-4 hover:shadow-xl ${
+        className || ""
+      }`}
       {...props}
     >
-      <div className="flex flex-col rounded-3xl border border-primary-300 overflow-hidden">
-        <img src={img.source} />
-        <div className="p-4">
-          <h2>{title}</h2>
-          <p>{description}</p>
-          <p className="my-2">
-            {tags.map((tag, index) => (
-              <Tag
-                key={index}
-                className={`${index !== tags.length && "mr-2"}`}
-                {...tag}
-              />
-            ))}
-          </p>
-        </div>
+      <div className="flex-1">
+        <h2>{title}</h2>
+        <h4>{subtitle}</h4>
+        <p>{description}</p>
+        <p className="text-danger-400">{annotation}</p>
+        <p className="my-2">
+          {tags.map((tag, index) => (
+            <Tag
+              key={index}
+              className={`${index !== tags.length && "mr-2"}`}
+              {...tag}
+            />
+          ))}
+        </p>
       </div>
+      <a href={link.href} target="_blank">
+        {link.label}
+      </a>
     </div>
   );
 };
