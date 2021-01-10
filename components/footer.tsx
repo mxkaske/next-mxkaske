a5070033c5adb76c6e8791d659ef11123fa33cc5
Maximilian Kaske <maximilian@kaske.org>
2021-01-10 15:07:16 +0100

    :wrench: extract sm into array

--- components/footer.tsx
+++ components/footer.tsx
@@ -1,17 +1,41 @@
 import React from "react";
 import Container from "./container";
-import Button from "./button";
 import Tag from "./tag";
+
+const sm: { label: string; href: string }[] = [
+  {
+    label: "LinkedIn",
+    href: "https://www.linkedin.com/in/maximilian-kaske-262227202/",
+  },
+  {
+    label: "Twitter",
+    href: "https://twitter.com/mxkaske",
+  },
+  {
+    label: "GitHub",
+    href: "https://github.com/maximiliankaske",
+  },
+  {
+    label: "Hashnode",
+    href: "https://mxkaske.hashnode.dev",
+  },
+];
 
 const Footer = () => {
   return (
     <footer className="bg-primary-200">
       <Container className="flex flex-col sm:flex-row space-between">
         <div className="flex-1">
-          <Button className="block mb-2">LinkedIn</Button>
-          <Button className="block mb-2">Twitter</Button>
-          <Button className="block mb-2">GitHub</Button>
-          <Button className="block">Hashnode</Button>
+          {sm.map(({ href, label }, index) => (
+            <a
+              key={index}
+              href={href}
+              target="_blank"
+              className="block mb-2 last:mb-0"
+            >
+              {label}
+            </a>
+          ))}
         </div>
         <div className="flex flex-col items-end justify-end text-xs">
           <p className="pb-1">
@@ -21,10 +45,10 @@ const Footer = () => {
           <p className="pb-1">
             build with
             <Tag label="nextjs" className="ml-1" />
+            <Tag label="tailwindcss" className="block ml-1" />
+            <Tag label="framer" className="block ml-1" />
           </p>
-          <Tag label="tailwindcss" className="block mb-1" />
-          <Tag label="framer" className="block mb-1" />
-          <p className="pb-1">
+          <p>
             deployed on
             <Tag label="vercel" className="ml-1" />
           </p>
