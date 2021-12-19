cb812b5d4a07c7a163b7f8478f0da1b75d431f5f
Maximilian Kaske <maximilian@kaske.org>
2021-12-19 23:16:41 +0100

    chore: update typography styles :art:

--- src/pages/blog/[slug].tsx
+++ src/pages/blog/[slug].tsx
@@ -1,27 +1,28 @@
 import { allPosts } from ".contentlayer/data";
 import type { Post } from ".contentlayer/types";
-import { InferGetStaticPropsType } from "next";
+import { GetStaticPropsContext, InferGetStaticPropsType } from "next";
 
 export default function Post({
   post,
 }: InferGetStaticPropsType<typeof getStaticProps>) {
   return (
-    <div>
-      <h1>{post.slug}</h1>
-      <div dangerouslySetInnerHTML={{ __html: post.body.html }} />
+    <div className="container p-4 mx-auto">
+      <div
+        className="prose dark:prose-invert hover:prose-a:no-underline marker:prose-ul:text-indigo-500 dark:prose-ul:text-gray-300 prose-ul:text-gray-700 prose-img:rounded-md prose-blockquote:border-indigo-500 prose-blockquote:rounded"
+        dangerouslySetInnerHTML={{ __html: post.body.html }}
+      />
     </div>
   );
 }
 
 export async function getStaticPaths() {
-  console.log(allPosts);
   return {
     paths: allPosts.map(({ slug }) => ({ params: { slug } })),
     fallback: false,
   };
 }
 
-export async function getStaticProps({ params }) {
+export async function getStaticProps({ params }: GetStaticPropsContext) {
   const post = allPosts.find((post) => post.slug === params.slug);
   return { props: { post } };
 }
