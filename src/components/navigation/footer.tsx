b0295c70c6b1a98e9616ea66929d422d1dfdbc6a
Maximilian Kaske <maximilian@kaske.org>
2021-12-20 22:22:20 +0100

    chore: remove unused code :coffin:

--- src/components/navigation/footer.tsx
+++ src/components/navigation/footer.tsx
@@ -1,7 +1,5 @@
 import React from "react";
 import Container from "../common/container";
-import Emoji from "../ui/emoji";
-import Tag from "../ui/tag";
 
 const sm: { label: string; href: string }[] = [
   {
@@ -24,42 +22,21 @@ const sm: { label: string; href: string }[] = [
 
 const Footer = () => {
   return (
-    <footer className="bg-indigo-100 dark:bg-gray-600">
+    <footer>
       <Container>
-        <div className="flex flex-col mb-4 sm:flex-row space-between">
-          <div className="flex-1">
-            {sm.map(({ href, label }, index) => (
-              <a
-                key={index}
-                href={href}
-                target="_blank"
-                className="block mb-2 last:mb-0"
-                rel="noreferrer"
-              >
-                {label}
-              </a>
-            ))}
-          </div>
-          <div className="flex flex-col items-end justify-end text-xs">
-            <p>
-              designed by
-              <Tag label="mxkaske" className="ml-1" />
-            </p>
-            <p>
-              build with <Tag label="nextjs" />
-              <Tag label="tailwindcss" />
-              <Tag label="framer" />
-            </p>
-            <p>
-              deployed on
-              <Tag label="vercel" className="ml-1" />
-            </p>
-          </div>
+        <div className="flex justify-center space-x-2">
+          {sm.map(({ href, label }, index) => (
+            <a
+              key={index}
+              href={href}
+              target="_blank"
+              className="block mb-2 last:mb-0"
+              rel="noreferrer"
+            >
+              {label}
+            </a>
+          ))}
         </div>
-        <p className="my-2 text-center">
-          <span className="text-sm">{`</>`}</span> with{" "}
-          <Emoji label="red heart" symbol="❤️" /> | 2021 ©
-        </p>
       </Container>
     </footer>
   );
