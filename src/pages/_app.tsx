b2d3118cb11dc2204f10115c8614314a086b144a
Maximilian Kaske <maximilian@kaske.org>
2021-12-22 20:22:53 +0100

    feat: add better SEO support

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,30 +1,18 @@
 import "@/styles/globals.css";
 import { ThemeProvider } from "next-themes";
-import Head from "next/head";
 import { SWRConfig } from "swr";
 import swrConfig from "swr.config";
+import { DefaultSeo } from "next-seo";
+import SEO from "next-seo.config";
 
 function MyApp({ Component, pageProps }) {
   return (
-    <>
-      {/* TODO: use next-seo instead */}
-      <Head>
-        <title>mxkaske</title>
-        <link rel="icon" href="/favicon.ico" />
-        <meta name="description" content="my portfolio" />
-        <meta property="og:image" content="/max.png" />
-        <meta property="og:title" content="mxkaske" />
-        <meta property="og:description" content="my porfolio" />
-        <meta property="og:type" content="website" />
-        <meta property="og:url" content="http://mxkaske.dev" />
-        <meta property="og:site_name" content="mxkaske" />
-      </Head>
-      <SWRConfig value={swrConfig}>
-        <ThemeProvider attribute="class">
-          <Component {...pageProps} />
-        </ThemeProvider>
-      </SWRConfig>
-    </>
+    <SWRConfig value={swrConfig}>
+      <DefaultSeo {...SEO} />
+      <ThemeProvider attribute="class">
+        <Component {...pageProps} />
+      </ThemeProvider>
+    </SWRConfig>
   );
 }
 
