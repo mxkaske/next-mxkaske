6500576288980b4cdf790e1000a2f79bc0d5f3be
Maximilian Kaske <maximilian@kaske.org>
2021-12-26 21:20:14 +0100

    chore: add little seo to post :mag:

---
+++ content/writing/website-redesign-2021.md
@@ -0,0 +1,8 @@
+---
+title: "Website redesign end of the year"
+date: "2021-12-26"
+excerpt: "Applying a minima design after learnings."
+highlight: true,
+---
+
+## Website redesign end of the year
--- src/pages/writing/[slug].tsx
+++ src/pages/writing/[slug].tsx
@@ -3,12 +3,14 @@ import type { Post } from ".contentlayer/types";
 import Details from "@/components/blog/details";
 import Layout from "@/components/common/layout";
 import { GetStaticPropsContext, InferGetStaticPropsType } from "next";
+import { NextSeo } from "next-seo";
 
 export default function Post({
   post,
 }: InferGetStaticPropsType<typeof getStaticProps>) {
   return (
     <Layout>
+      <NextSeo title={post.title} description={post.excerpt} />
       <Details post={post} />
       <div
         className="py-16 prose dark:prose-invert prose-a:decoration-gray-500 hover:prose-a:decoration-black dark:hover:prose-a:decoration-white prose-img:rounded-md prose-blockquote:rounded prose-em:text-gray-900 dark:prose-em:text-white prose-em:font-light"
