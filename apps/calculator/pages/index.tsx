e1dba706a8af97c1c42227a22b3fae7d72ccf2f7
Maximilian Kaske <maximilian@kaske.org>
2022-02-17 20:37:55 +0100

    chore: init nextjs

---
+++ apps/calculator/pages/index.tsx
@@ -0,0 +1,12 @@
+import type { NextPage } from "next";
+import { Input } from "ui";
+
+const Home: NextPage = () => {
+  return (
+    <div className="flex items-center justify-center min-h-screen">
+      <Input />
+    </div>
+  );
+};
+
+export default Home;
