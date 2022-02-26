7be00576ee0ba083852303a28ecb74592cf6df59
Maximilian Kaske <maximilian@kaske.org>
2022-02-26 14:15:55 +0100

    chore: range css

--- packages/ui/src/range.tsx
+++ packages/ui/src/range.tsx
@@ -5,7 +5,16 @@ export interface RangeProps
   extends React.InputHTMLAttributes<HTMLInputElement> {}
 
 const Range: React.FC<RangeProps> = ({ children, className, ...props }) => {
-  return <input type="range" className={cn("", className)} {...props} />;
+  return (
+    <input
+      type="range"
+      className={cn(
+        "w-full h-2 bg-gray-300 rounded appearance-none",
+        className
+      )}
+      {...props}
+    />
+  );
 };
 
 export default Range;
