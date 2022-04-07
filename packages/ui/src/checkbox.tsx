c5b1c1e5124712780f66255666fbc3de110d8a30
Maximilian Kaske <maximilian@kaske.org>
2022-04-07 17:48:36 +0200

    chore: design change

--- packages/ui/src/checkbox.tsx
+++ packages/ui/src/checkbox.tsx
@@ -2,7 +2,7 @@ import * as React from "react";
 import cn from "classnames";
 
 const styles = {
-  base: "focus:ring-indigo-500 h-4 w-4 text-gray-600 border-gray-300 rounded",
+  base: "focus:ring-indigo-500 h-4 w-4 text-gray-900 border-gray-300 rounded",
 };
 
 export interface CheckboxProps
