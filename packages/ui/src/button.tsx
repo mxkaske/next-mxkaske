831d0dc7ee842ead536e343a4d740aa1c8ac3e69
Maximilian Kaske <maximilian@kaske.org>
2022-04-08 17:01:47 +0200

    feat: invert button color

--- packages/ui/src/button.tsx
+++ packages/ui/src/button.tsx
@@ -2,24 +2,35 @@ import * as React from "react";
 import cn from "classnames";
 
 const styles = {
-  base: "px-3 py-2 text-black bg-transparent border border-gray-300 rounded-md dark:text-white hover:border-gray-400 dark:hover:border-gray-600 dark:border-gray-700",
+  base: "px-3 py-2 border border-gray-300 rounded-md hover:border-gray-400 dark:hover:border-gray-600 dark:border-gray-700",
+  variant: {
+    default: "text-black dark:text-white bg-transparent",
+    invert: "bg-gray-900 text-white",
+  },
   rounded: "rounded-full",
 };
 
 export interface ButtonProps extends React.HTMLAttributes<HTMLButtonElement> {
   label?: string;
+  variant?: keyof typeof styles.variant;
   rounded?: boolean;
 }
 const Button = ({
   children,
   label,
   className,
+  variant = "default",
   rounded,
   ...props
 }: ButtonProps) => {
   return (
     <button
-      className={cn(styles.base, { [styles.rounded]: rounded }, className)}
+      className={cn(
+        styles.base,
+        styles.variant[variant],
+        { [styles.rounded]: rounded },
+        className
+      )}
       {...props}
     >
       {label || children}
