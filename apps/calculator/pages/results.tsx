53463b6285a4b07227c26e3d7dc1cff6479c4178
Maximilian Kaske <maximilian@kaske.org>
2022-04-14 17:49:51 +0200

    chore: small changes on results

--- apps/calculator/pages/results.tsx
+++ apps/calculator/pages/results.tsx
@@ -2,17 +2,15 @@ import React from "react";
 import { Heading } from "ui";
 import ActionBar from "../components/common/ActionBar";
 import Layout from "../components/common/Layout";
-import ResultBar from "../components/visualization/ResultBar";
 import ResultByType from "../components/visualization/ResultByType";
 
 const Results = () => {
   return (
     <Layout>
       <div className="flex-1 flex flex-col items-center justify-center min-h-max">
-        <Heading>Results</Heading>
         <div className="w-full flex flex-col space-y-6">
+          <Heading as="h1">Results</Heading>
           <ResultByType />
-          <ResultBar />
         </div>
       </div>
       <ActionBar />
