7888f3f4bbd6448688f7a139e6a5186fcea8798e
Maximilian Kaske <maximilian@kaske.org>
2022-08-28 09:24:35 +0200

    feat: add types to next.config

--- apps/web/next.config.js
+++ apps/web/next.config.js
@@ -1,3 +1,9 @@
+/**
+ * @type {import('next').NextConfig}
+ */
 const { withContentlayer } = require("next-contentlayer");
+const nextConfig = {
+  /* config options here */
+};
 
-module.exports = withContentlayer({});
+module.exports = withContentlayer(nextConfig);
