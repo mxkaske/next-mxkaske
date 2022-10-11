49a99df918309841fa34ee2046879d0df4727cbf
Maximilian Kaske <maximilian@kaske.org>
2022-10-11 16:49:55 +0200

    wip

--- apps/web/content/cv/education/002-math-studies.md
+++ apps/web/content/cv/education/002-math-studies.md
@@ -6,4 +6,4 @@ what: "B.Sc. in Mathematics"
 where: "TU Berlin"
 ---
 
-I studied **Mathematics** at the _Technische Universität Berlin_ where I specialised in Algorithmic Discrete Mathematics (ADM). My Bachelor Thesis was about _Optimization of large-scale Order-Stock-Container-Allocation_, a specific multi-dimensional knapsack problem. The algorithm supporting the work has been written in Python.
+I studied **Mathematics** at the _Technische Universität Berlin_ where I specialised in Algorithmic Discrete Mathematics (ADM). My Bachelor Thesis was about _Optimization of large-scale Order-Stock-Container-Allocation_, a specific multi-dimensional knapsack problem. The algorithm supporting the work has been written in **Python**.
--- apps/web/src/pages/cv.tsx
+++ apps/web/src/pages/cv.tsx
@@ -9,7 +9,16 @@ import React from "react";
 import { FiPrinter } from "react-icons/fi";
 import { Button, Heading, Link, Text } from "ui";
 
-const selfDescription = `I’m Max, a self-taught Fullstack Developer working on Web- and App projects in JS/TS. I’m passionate about the newest technologies, clean design, productivity tools, and code architecture. I'm experienced with React & Next.js for Web development, React Native & Expo for App development, serverless databases like upstash (Redis), planetscale (MySQL) and deployments on vercel.`;
+const self = {
+  description: `I’m Max, a self-taught Fullstack Developer working on Web- and App projects in JS/TS. I’m passionate about the newest technologies, clean design, productivity tools, and code architecture. I'm experienced with React & Next.js for Web development, React Native & Expo for App development and serverless infrastructure like upstash, planetscale or vercel.`,
+  informations: {
+    birthday: "7th August 1995",
+    languages: "native in French & German, fluent in English",
+    location: "Berlin, Germany",
+    hobbies: "cycling, bouldering, traveling, learning, teaching",
+  },
+};
+
 const styles = {
   prose:
     "prose dark:prose-invert prose-a:decoration-gray-500 hover:prose-a:decoration-black dark:hover:prose-a:decoration-white prose-img:rounded-md prose-blockquote:rounded prose-em:text-gray-900 dark:prose-em:text-white prose-em:font-light",
@@ -48,11 +57,13 @@ const CVPage = ({ events }: InferGetStaticPropsType<typeof getStaticProps>) => {
         </Grid.Left>
         <Grid.Right>
           <Heading>Maximilian Kaske</Heading>
-          <div className="flex space-x-6 justify-between mb-6">
-            <Text className="text-gray-800 dark:text-gray-200 italic font-extralight after:content-['”'] after:ml-0.5 after:text-gray-500 before:content-['“'] before:mr-0.5 before:text-gray-500">
-              {selfDescription}
-            </Text>
-            <div className="flex-shrink-0 h-40 w-40 relative rounded-full overflow-hidden">
+          <div className="flex flex-col md:flex-row gap-3 justify-between mb-6">
+            <div>
+              <Text className="text-gray-800 dark:text-gray-200 italic font-extralight after:content-['”'] after:ml-0.5 after:text-gray-500 before:content-['“'] before:mr-0.5 before:text-gray-500">
+                {self.description}
+              </Text>
+            </div>
+            <div className="-order-1 md:order-2 self-center flex-shrink-0 h-40 w-40 relative rounded-full overflow-hidden">
               <Image
                 src="/profile.jpg"
                 layout="fill"
@@ -63,15 +74,14 @@ const CVPage = ({ events }: InferGetStaticPropsType<typeof getStaticProps>) => {
           </div>
           <div className={styles.prose}>
             <p className="mb-0">
-              <strong>Birthday</strong>: 7th August 1995
-              <br />
-              <strong>Languages</strong>: native in French & German, fluent in
-              English
-              <br />
-              <strong>Location</strong>: Berlin
-              <br />
-              <strong>Hobbies</strong>: cycling, bouldering, traveling,
-              learning, teaching
+              {Object.entries(self.informations).map(([key, value]) => {
+                return (
+                  <React.Fragment key={key}>
+                    <strong className="capitalize">{key}</strong>: {value}
+                    <br />
+                  </React.Fragment>
+                );
+              })}
             </p>
           </div>
         </Grid.Right>
