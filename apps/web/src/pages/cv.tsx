f18a2ebe328970fba4b3c529429c3f48eee9088d
Maximilian Kaske <maximilian@kaske.org>
2022-10-18 14:42:18 +0200

    chore: typo

--- apps/web/src/pages/cv.tsx
+++ apps/web/src/pages/cv.tsx
@@ -12,7 +12,7 @@ import { Button, Heading, Link, Text } from "ui";
 import NextLink from "next/link";
 
 const self = {
-  description: `Hi! I’m Max, a Fullstack Developer working on Web- and App projects in JS/TS. I’m passionate about the newest frameworks, clean design, productivity tools, and code architecture. I'm experienced with React & Next.js for WebDevelopment, React Native & Expo for App Development and serverless infrastructure.`,
+  description: `Hi! I’m Max, a Fullstack Developer working on Web- and App projects in JS/TS. I’m passionate about the newest frameworks, clean design, productivity tools, and code architecture. I'm experienced with React & Next.js for Web Development, React Native & Expo for App Development and serverless infrastructure.`,
   informations: {
     birthday: "7th August 1995",
     languages: "native in French & German, fluent in English",
