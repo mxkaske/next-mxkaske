be42818d21939b1e358647f2d2dc070de43b20b6
Maximilian Kaske <maximilian@kaske.org>
2022-10-26 12:10:22 +0200

    wip

--- apps/web/src/middleware.ts
+++ apps/web/src/middleware.ts
@@ -1,20 +1,27 @@
 import { Redis } from "@upstash/redis";
 import { NextRequest, NextResponse } from "next/server";
 
+// REMINDER: if not solving the issue: useEffect once on CSR and add an api/views/[slug] route to redis.incr.
+
 const redis = Redis.fromEnv();
 
 const handler = (req: NextRequest) => {
-  // REMINDER: this will also allow pathnames like `/writing/unknown-path`.
-  // To avoid that, we could find the allPosts post.flattendPath that matches the slug.
-  if (req.nextUrl.pathname.startsWith("/writing/")) {
+  try {
     const slug = req.nextUrl.searchParams.get("slug");
+    console.log({ slug, nextUrl: req.nextUrl.searchParams.getAll });
     // FIXME:
-    console.log({ nextUrl: req.nextUrl, slug });
     if (!!slug) {
+      console.log(slug);
       redis.incr(`views:${slug}`);
     }
+  } catch (e) {
+    console.error(e);
   }
-  return NextResponse.next();
+  // return NextResponse.next();
+};
+
+export const config = {
+  matcher: "/writing/:slug*",
 };
 
 export default handler;
