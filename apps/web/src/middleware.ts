64f0eef4eb5796385ea79e0c641bcd4af4a5cfd2
Maximilian Kaske <maximilian@kaske.org>
2022-11-09 12:03:14 +0100

    fix: header in middleware

--- apps/web/src/middleware.ts
+++ apps/web/src/middleware.ts
@@ -1,26 +1,16 @@
 import { Redis } from "@upstash/redis";
 import { NextRequest, NextFetchEvent, NextResponse } from "next/server";
 
-// REMINDER: if not solving the issue: useEffect once on CSR and add an api/views/[slug] route to redis.incr.
-
 const redis = Redis.fromEnv();
 
-const handler = (req: NextRequest, event: NextFetchEvent) => {
+const handler = (req: NextRequest) => {
   try {
     const slug = req.nextUrl.searchParams.get("slug");
-    console.log({
-      slug,
-      purpose: req.headers.get("purpose"),
-      headers: req.headers,
-    });
-    // FIXME:
-    if (!!slug) {
-      // event.waitUntil(
-      //   (async () => {
-      //     console.log(slug);
-      //     redis.incr(`views:${slug}`);
-      //   })()
-      // );
+
+    const purpose = req.headers.get("purpose");
+
+    if (!!slug && purpose !== "prefetch") {
+      redis.incr(`views:${slug}`);
     }
   } catch (e) {
     console.error(e);
