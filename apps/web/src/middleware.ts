6286b9a9819872ea356be85ddb22b526aadde993
Maximilian Kaske <maximilian@kaske.org>
2023-01-14 17:11:20 +0100

    fix: first page load

--- apps/web/src/middleware.ts
+++ apps/web/src/middleware.ts
@@ -3,11 +3,36 @@ import { NextRequest, NextFetchEvent, NextResponse } from "next/server";
 
 const redis = Redis.fromEnv();
 
+const PATTERNS = [
+  [
+    // @ts-ignore cannot find ts type
+    new URLPattern({ pathname: "/writing/:slug" }),
+    ({ pathname }) => pathname.groups,
+  ],
+];
+
+const params = (url: string) => {
+  const input = url.split("?")[0];
+  let result = {};
+
+  for (const [pattern, handler] of PATTERNS) {
+    const patternResult = pattern.exec(input);
+    if (patternResult !== null && "pathname" in patternResult) {
+      result = handler(patternResult);
+      break;
+    }
+  }
+  return result;
+};
+
 const handler = (req: NextRequest) => {
   try {
-    const slug = req.nextUrl.searchParams.get("slug");
-
+    // BEFORE: missing `slug` on fresh first page load (e.g. refresh)
+    // const slug = req.nextUrl.searchParams.get("slug");
+    // AFTER:
+    const { slug } = params(req.url) as { slug?: string };
     const purpose = req.headers.get("Purpose");
+
     console.log({ slug, purpose, method: req.method });
 
     if (!!slug && purpose !== "prefetch") {
